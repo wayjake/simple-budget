bd876c88545c6269ac2712f7f6deea9c663a7ffe
Jacob Berg <jake@dubsado.com>
2024-07-12 18:17:44 -0700

    mostly prettier changes

--- app/components/DarkToggle.tsx
+++ app/components/DarkToggle.tsx
@@ -1,28 +1,28 @@
-import { useEffect } from "react";
+import { useEffect } from 'react'
 
 const DarkToggle = ({ className }) => {
-    useEffect(() => {
-        const isDarkMode = localStorage.getItem("darkMode") === "true";
-        if (isDarkMode) {
-            document.documentElement.classList.add("dark");
-        } else {
-            document.documentElement.classList.remove("dark");
-        }
-    }, []);
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem('darkMode') === 'true'
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [])
 
-    const handleToggleDarkMode = () => {
-        const isDarkMode = document.documentElement.classList.toggle("dark");
-        localStorage.setItem("darkMode", isDarkMode.toString());
-    };
+  const handleToggleDarkMode = () => {
+    const isDarkMode = document.documentElement.classList.toggle('dark')
+    localStorage.setItem('darkMode', isDarkMode.toString())
+  }
 
-    return (
-        <button
-            onClick={handleToggleDarkMode}
-            className={`${className} bg-gray-800 text-white py-1 px-2 rounded`}
-        >
-            Toggle Dark Mode
-        </button>
-    )
+  return (
+    <button
+      onClick={handleToggleDarkMode}
+      className={`${className} bg-gray-800 text-white py-1 px-2 rounded`}
+    >
+      Toggle Dark Mode
+    </button>
+  )
 }
 
 export default DarkToggle
